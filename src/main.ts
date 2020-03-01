1eb76bea11a8345e4436520b42721faed8c2af82
Luca Terrazzan <luca.terraz@gmail.com>
2020-03-01 11:33:13 +0100

    mah

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-class Main {
-    constructor() {
-        console.log('got');
-    }
+import { Product } from './product';
+
+const sampleProducts: string[] = [
+    'https://www.amazon.it/gp/product/B07Y8YWTFL',
+    'https://www.amazon.it/Xiaomi-Smartphone-2280x1080-Snapdragon-Fotocamera/dp/B008I0YEWE'
+];
+
+async function main() {
+    const product = new Product(sampleProducts[1], 'note 8 pro');
+    const price: number = await product.getPrice();
+    console.log(price);
 
 }
+
+main()
